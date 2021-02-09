9152b4b9412694a4a8f5a950bb138bee04d81603
Jon C <nightydog27@hotmail.com>
2021-02-09 03:00:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+function destructivelyRemoveElementFromBeginningOfArray(array,element) {
+  array.shift(element)
   return array
 }
