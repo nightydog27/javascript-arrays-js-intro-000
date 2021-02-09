be75e643e0ce8be2a47cd22c250fd35918e3312f
Jon C <nightydog27@hotmail.com>
2021-02-09 03:17:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToEndOfArray(array, element) {
   return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array) {
   array.push(element)
   return array
 }
